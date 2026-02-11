da7ee6010282d4bfed19923a92539b9975a4caba
Alex Berezko Mac <berezko33@gmail.com>
2026-02-11 21:00:32 +0300

    creation of a temporary directory and a file

--- tests/path_size_test.go
+++ tests/path_size_test.go
@@ -12,11 +12,12 @@ import (
 
 func TestGetPathSize_File(t *testing.T) {
 	testDir := t.TempDir()
-
 	testFile := filepath.Join(testDir, "testFile.txt")
+	testContent := []byte("Hello World, Hello Hexlet!")
+	err := os.WriteFile(testFile, testContent, 0644)
 
-	size, err := code.GetPathSize(testFile, true, false, false)
-	require.NoError(t, err)
+	size, errSize := code.GetPathSize(testFile, true, false, false)
+	require.NoError(t, errSize)
 
 	info, err := os.Stat(testFile)
 	require.NoError(t, err)
